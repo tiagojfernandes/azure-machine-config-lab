59f2b7694fb9fee115ccd933a9c914c8397e2307
Tiago Fernandes <tifernan@microsoft.com>
2025-12-12 12:22:26 +0000

    update providers

--- envs/dev/providers.tf
+++ envs/dev/providers.tf
@@ -1,12 +1,12 @@
 # Provider Configuration
 
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.9.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.80"
+      version = "~> 4.14"
     }
   }
 }
